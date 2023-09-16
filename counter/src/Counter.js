b44eb16e9a2bad5f4704e5ca3ded62cb06133ae4
akshatgg <akshatg9636@gmail.com>
2023-09-17 03:23:54 +0545

    Counter

--- counter/src/Counter.js
+++ counter/src/Counter.js
@@ -1,3 +1,5 @@
+
+import './Counter.css'
 import{ useState } from 'react';
 function Counter(){
 
@@ -5,22 +7,29 @@ const [x,setX]= useState(0);
 const [y,setY]= useState(0);
 
 return (
-    <>  
+  <body>
+    <b2>
+    <h3 >Counter App</h3>
+    <nav>  
+<h5>Calculator</h5>
+<br/>
     Count X: {x}
     <br/>
-    <button onClick={()=> setX(x+1)}>Increment</button>
-    <button onClick={()=> setX(x-1)}>Decrement</button>
+    <button onClick={()=> setX(x+1) }className="IX">Increment</button>
+    <button onClick={()=> setX(x-1)} className="DX">Decrement</button>
     <br/>
-    <button onClick={()=> setX(0)}>Reset</button>
+    <button onClick={()=> setX(0)} className="RX">Reset</button>
     <br/>
     Count Y: {y}
     <br/>
-    <button onClick={()=> setY(y+1)}>Increment</button>
-    <button onClick={()=> setY(y-1)}>Decrement</button>
+    <button onClick={()=> setY(y+1) }className="IY">Increment</button>
+    <button onClick={()=> setY(y-1)}className="DY">Decrement</button>
     <br/>
-    <button onClick={()=> setY(0)}>Reset</button>
+    <button onClick={()=> setY(0)}className="RX">Reset</button>
 
-    </>
+    </nav>
+    </b2>
+    </body>
 )
 }
 export default Counter;
