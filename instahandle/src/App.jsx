62ae472e64b6902cdc7e92992b419edc00f0d5b2
akshatgg <akshatg9636@gmail.com>
2023-09-21 00:43:17 +0545

    instahandle

---
+++ instahandle/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import { useState } from "react"
+import logo from "./assets/instagram.png"
+function App() {
+var [login,setlogin]=useState(true)
+const switchlogin=()=>{
+  setlogin(!login)
+}
+  return (   
+      <div className="container">
+
+  <img src={logo} alt="" className="image"/>
+  <input hidden={login} name="" placeholder="Mobile number or Email" type ="text"/>
+  <input hidden={login} name="" placeholder="First Name" type ="text"/>
+  <input hidden={login} name="" placeholder="Last Name" type ="text"/>
+  <input name="" placeholder="Phone number or username or email " type ="text"/>
+  <input  name="" placeholder="Password" type ="password"/>
+      <button >{login ? "Sign in"  :"Sign up"}</button>
+      <div id="footer">
+    {!login ? "Dont't have an account":"Have an account"} <span onClick={switchlogin}>{login ? "Sign up" : "Log in"}</span>
+      </div>
+
+    
+      </div>
+  )
+}
+
+export default App
